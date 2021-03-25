62d6ac43df3634d74c1f6bb7ce08e560988d722c
imran-salim <imransalim08@gmail.com>
2021-03-25 19:28:07 +1300

    implement game rules and add more control options

--- src/Game.js
+++ src/Game.js
@@ -6,13 +6,21 @@ const CELL_SIZE = 20;
 const WIDTH = 800;
 const HEIGHT = 600;
 
-class Game extends React.Component {
-    state = {
-        cells: [],
-        interval: 100,
-        isRunning: false,
-    }
 
+class Cell extends React.Component {
+    render() {
+        const { x, y } = this.props;
+        return (
+            <div className="Cell"
+                 style={{ left: `${CELL_SIZE * x + 1}px`,
+                          top: `${CELL_SIZE * y + 1}px`,
+                          width: `${CELL_SIZE - 1}px`,
+                          height: `${CELL_SIZE - 1}px` }}/>
+        );
+    }
+}
+
+class Game extends React.Component {
     constructor() {
         super();
         this.rows = HEIGHT / CELL_SIZE;
@@ -20,26 +28,36 @@ class Game extends React.Component {
         this.board = this.makeEmptyBoard();
     }
 
+    state = {
+        cells: [],
+        interval: 100,
+        isRunning: false,
+    }
+
     makeEmptyBoard() {
         let board = [];
+
         for (let y = 0; y < this.rows; y++) {
             board[y] = [];
             for (let x = 0; x < this.cols; x++) {
                 board[y][x] = false;
             }
         }
+
         return board;
     }
 
     makeCells() {
         let cells = [];
+
         for (let y = 0; y < this.rows; y++) {
             for (let x = 0; x < this.cols; x++) {
                 if (this.board[y][x]) {
-                    cells.push({ x, y, });
+                    cells.push({ x, y });
                 }
             }
         }
+
         return cells;
     }
 
@@ -86,7 +104,23 @@ class Game extends React.Component {
         console.log("running iteration");
         let newBoard = this.makeEmptyBoard();
 
-        // add logic for iterations
+        // Implementation of rules in Conway's Game of Life
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                let neighbors = this.calculateNeighbors(this.board, x, y);
+                if (this.board[y][x]) {
+                    if (neighbors === 2 || neighbors === 3) {
+                        newBoard[y][x] = true;
+                    } else {
+                        newBoard[y][x] = false;
+                    }
+                } else {
+                    if (!this.board[y][x] && neighbors === 3) {
+                        newBoard[y][x] = true;
+                    }
+                }
+            }
+        }
 
         this.board = newBoard;
         this.setState({ cells: this.makeCells() });
@@ -96,8 +130,40 @@ class Game extends React.Component {
         },  this.state.interval);
     }
 
+    calculateNeighbors(board, x, y) {
+        let neighbors = 0;
+        const coordinates = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
+        
+        for (let i = 0; i < coordinates.length; i++) {
+            const coordinate = coordinates[i];
+            let y1 = y + coordinate[0]
+            let x1 = x + coordinate[1];
+
+            if (x1 >= 0 && x1 < this.cols && y1 >= 0 && y1 < this.rows && board[y1][x1]) {
+                neighbors++;
+            }
+        }
+
+        return neighbors;
+    }
+
     handleIntervalChange = (event) => {
         this.setState({ interval: event.target.value });
+    }
+
+    handleClear = () => {
+        this.board = this.makeEmptyBoard();
+        this.setState({ cells: this.makeCells() });
+    }
+
+    handleRandom = () => {
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                this.board[y][x] = (Math.random() >= 0.5);
+            }
+        }
+
+        this.setState({ cells: this.makeCells() });
     }
         
     render() {
@@ -117,28 +183,17 @@ class Game extends React.Component {
                     )}
                 </div>
                 <div className="controls">
-                    Update every
+                    Update every&nbsp;
                         <input value={this.state.interval}
-                               onChange={this.handleIntervalChange} /> msec
+                               onChange={this.handleIntervalChange} />&nbsp;msec<br/><br/>
                     {isRunning ?
                         <button className="button" onClick={this.stopGame}>Stop</button> : 
                         <button className="button" onClick={this.runGame}>Run</button>
-                    }
+                    }&nbsp;
+                    <button className="button" onClick={this.handleRandom}>Random</button>&nbsp;
+                    <button className="button" onClick={this.handleClear}>Clear</button>
                 </div>
             </div>
-        );
-    }
-}
-
-class Cell extends React.Component {
-    render() {
-        const { x, y } = this.props;
-        return (
-            <div className="Cell"
-                 style={{ left: `${CELL_SIZE * x + 1}px`,
-                          top: `${CELL_SIZE * y + 1}px`,
-                          width: `${CELL_SIZE - 1}px`,
-                          height: `${CELL_SIZE - 1}px` }}/>
         );
     }
 }
